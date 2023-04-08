91df12cf83c481187e800475ff43d32bf1d8b9d6
abhi1506manu <abhi2019manu@gmail.com>
2023-04-08 21:59:59 +0530

    backend api created now adding frontend files

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,58 @@ const MORALIS_API_KEY = process.env.API_KEY;
 app.get("/getetherprice", async (req, res) => {
   try {
     const response = await Moralis.EvmApi.token.getTokenPrice({
-      address:"0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-      chain:"0x1",
+      address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+      chain: "0x1",
     });
-    console.log(response.toJSON())
+    // console.log(response.toJSON());
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(`Somthing went wrong ${error}`);
+    return res.status(400).json();
+  }
+});
+
+//getblockInformtion
+app.get("/blockinfo", async (req, res) => {
+  try {
+    const latestBlock = await Moralis.EvmApi.block.getDateToBlock({
+      date: Date.now(),
+      chain: "0x1",
+    });
+    let blockNrOrParentHash = latestBlock.toJSON().block;
+    let previousBlockInfo = [];
+
+    for (let i = 0; i < 5; i++) {
+      const previousBlockNrs = await Moralis.EvmApi.block.getBlock({
+        chain: "0x1",
+        blockNumberOrHash: blockNrOrParentHash,
+      });
+      blockNrOrParentHash = previousBlockNrs.toJSON().parent_hash;
+      if (i == 0) {
+        previousBlockInfo.push({
+          transactions: previousBlockNrs.toJSON().transactions.map((i) => {
+            return {
+              transactionHash: i.hash,
+              time: i.block_timestamp,
+              fromAddress: i.from_address,
+              toAddress: i.to_address,
+              value: i.value,
+            };
+          }),
+        });
+      }
+      previousBlockInfo.push({
+        blockNumber: previousBlockNrs.toJSON.number,
+        totalTransactions: previousBlockNrs.toJSON().transaction_count,
+        gasUsed: previousBlockNrs.toJSON().gas_used,
+        miner: previousBlockNrs.toJSON().miner,
+        time: previousBlockNrs.toJSON().timestamp,
+      });
+    }
+    const response = {
+      latestBlock: latestBlock.toJSON().block,
+      previousBlockInfo,
+    };
     return res.status(200).json(response);
   } catch (error) {
     console.log(`Somthing went wrong ${error}`);
@@ -32,11 +80,11 @@ app.get("/address", async (req, res) => {
     // const address = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
     const chain = "0x1";
     // console.log(` I am query${query}`)
-    console.log(query)
+    console.log(query);
 
     const response =
       await Moralis.EvmApi.transaction.getWalletTransactionsVerbose({
-        address:query.address,
+        address: query.address,
         chain,
       });
 
